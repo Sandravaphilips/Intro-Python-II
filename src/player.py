7895f7893f1dcda5dd82e2fadfc79a89ad73cde1
Sandrava Philips <lacasandra1@gmail.com>
2020-02-12 17:57:22 +0100

    Built the Player class to specifications

--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+
+class Player():
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+        
+        
+    def update_room(self, new_room):
+        self.current_room = new_room
+        
