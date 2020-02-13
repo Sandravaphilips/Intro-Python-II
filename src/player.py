886bc6261a119e40c2a0422ee98ca15d693e3b4b
Sandrava Philips <lacasandra1@gmail.com>
2020-02-13 16:39:37 +0100

    added add_item and remove_item methods to the Player class

--- src/player.py
+++ src/player.py
@@ -1,13 +1,18 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-from room import Room
 
 class Player():
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
-        
+        self.list = []
         
     def update_room(self, new_room):
         self.current_room = new_room
+
+    def add_item(self, item):
+        self.list.append(item)
+    
+    def remove_item(self, item):
+        del self.list[self.list.index(item)]
         
